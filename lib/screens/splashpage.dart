8dad345e87ca53d56d09d89b9dc3c841e8c9ea64
swathi1006 <swathisahadevan996@gmail.com>
2024-05-15 11:32:49 +0530

    commit

--- lib/screens/splashpage.dart
+++ lib/screens/splashpage.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
+import 'package:bagapp/screens/loginpage.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 3), () { });
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+    });
     return Scaffold(
       backgroundColor: Colors.red[900],
       body: Center(
---
+++ lib/utils/mycolors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyColors{
+  static Color? basicColor = Colors.red[800];
+}
